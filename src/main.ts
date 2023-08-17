9880548524b74da2b847c6f961311f731b5816bc
Da4ndo <contact@da4ndo.com>
2023-08-18 01:51:35 +0200

    fix: Port 3000 => 4000 to not conflict with nextjs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+require('dotenv').config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
